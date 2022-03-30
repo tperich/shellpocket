456a9e6dda036cae220cf15ab060bb10f60de4af
Tomislav Perić <toca91@gmail.com>
2022-03-30 18:41:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Shellpocket
+
+<img style="filter: invert(1)" src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/solid/terminal.svg" width="30" height="12"> Bring reverse shells with you.
+
+## Setting up
+
+1. Clone the repository from https://github.com/tperich/shellpocket
+2. Run `pip install -r requirements` to install dependencies
+3. Run `./shellpocket.py -h`
+
+## To do:
+ - [x] Load shells from a file
+ - [x] Send from choice menu to stdin
+ - [x] Replace url,port via args
+ - [ ] Separate by category for nicer printing
+ - [ ] Support for other entities (ex. useful commands)
+ - [ ] Local DB support
+ - [ ] Support adding/deleting/updating shells using the menu
+
+## Thanks:
+ - To MrPineMan for [Awesome-Reverse-Shell](https://github.com/MrPineMan/Awesome-Reverse-Shell) collection
---
+++ shellpocket.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import os
+import re
+import fcntl
+import termios
+import argparse
+
+import yaml
+from simple_term_menu import TerminalMenu
+
+DEFAULT_URL = "127.0.0.1"
+DEFAULT_PORT = 1337
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog=__package__, description="Your own shell pocket"
+    )
+    parser.add_argument(
+        "-i",
+        "--ip",
+        help=f"IP to use when generating a shell (default: {DEFAULT_URL})",
+        default=DEFAULT_URL,
+        type=str,
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        help=f"Port to use when generating a shell (default: {DEFAULT_PORT})",
+        default=DEFAULT_PORT,
+        type=int,
+    )
+    return parser.parse_args()
+
+
+def load_file() -> list:
+    with open("shells.yml", "r") as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+
+def pick_a_shell(shells: list) -> str:
+    pretty_shells = []
+    for category in shells:
+        for shell in category["shells"]:
+            pretty = f"[{category['category']}] {shell}"
+            pretty_shells.append(pretty)
+
+    terminal_menu = TerminalMenu(pretty_shells)
+    choice_index = terminal_menu.show()
+    if type(choice_index) != int: return
+
+    choice = pretty_shells[choice_index]
+    chosen_shell = re.findall("\].*", choice)[0].strip("] ")
+    return chosen_shell
+
+
+def replace_conn_info(shells: list, ip: str, port: int):
+    for category in shells:
+        for idx, shell in enumerate(category["shells"]):
+            new_shell = shell.replace("{{ip}}", ip).replace("{{port}}", str(port))
+            category["shells"][idx] = new_shell
+    return shells
+
+
+def send_to_stdin(string: str) -> None:
+    tty_path = "/proc/{}/fd/0".format(os.getpid())
+    with open(tty_path, "w") as tty_fd:
+        for char in string:
+            fcntl.ioctl(tty_fd, termios.TIOCSTI, char)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    shells = load_file()
+    shells = replace_conn_info(shells, args.ip, args.port)
+    shell = pick_a_shell(shells)
+    if shell: send_to_stdin(shell)
